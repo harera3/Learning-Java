4519f3380f6db4a9140007de64aed1695d49bd4c
Innocent Harelimana <harera3@gmail.com>
2022-03-21 22:19:34 -0500

    add folder ihh_javaguide ex 01

---
+++ ihh_javaguide/Ex0101JavaWeb.java
@@ -0,0 +1,15 @@
+package ihh_javaguide;
+/*
+ * 	This is the first of many exercises and projects.
+ *  Book: Java, a Beginer's Guide, 9Ed.
+ */
+
+public class Ex0101JavaWeb {
+
+	public static void main(String[] args) {
+		// This is my first Java program.
+		System.out.println("Java drives the Web.");
+
+	}
+
+}
