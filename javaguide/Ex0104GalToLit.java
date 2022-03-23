4d094093d08b10bd2159abb54ce7ef63404a5067
Innocent Harelimana <harera3@gmail.com>
2022-03-22 23:34:24 -0500

    add galtolit

---
+++ javaguide/Ex0104GalToLit.java
@@ -0,0 +1,22 @@
+package ihh_javaguide;
+
+/*
+ 	This is a useful example.
+ 	Converting gallons to liters (by 3.7854)
+ */
+
+public class Ex0104GalToLit {
+	public static void main(String[] args) {
+		double gallons;
+		double liters;
+		
+		gallons = 10;
+		
+		liters = gallons * 3.7854;
+		
+		System.out.println(gallons + " gallons is " + liters + " liters.");
+	}
+
+}
+
+
