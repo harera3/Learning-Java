120876379bd02c662919c5e75e0ee56e8856c8a7
Innocent Harelimana <harera3@gmail.com>
2022-03-28 20:50:23 -0500

    add method parameters

---
+++ javaguide/chapter04/Ex0403VehicleParam.java
@@ -0,0 +1,55 @@
+package ihh_javaguide04;
+
+/* 
+	Add a parameterized method that computes the 
+	fuel required for a given distance. 
+*/ 
+
+class Vehicle3 {
+	int passengers; // number of passengers 
+	int fuelcap;    // fuel capacity in gallons 
+	int mpg;        // fuel consumption in miles per gallon 
+	
+	// Return the range. 
+	int range() {
+		return mpg * fuelcap;
+	}
+	
+	// Compute fuel needed for a given distance. 
+	double fuelNeeded(int miles) { 
+		return (double) miles / mpg; 
+	  } 
+}
+
+public class Ex0403VehicleParam {
+	public static void main(String[] args) { 		
+	    Vehicle3 minivan = new Vehicle3(); 
+	    Vehicle3 sportscar = new Vehicle3(); 
+	    double gallons; 
+	    int dist = 252; 
+	 
+	    // assign values to fields in minivan 
+	    minivan.passengers = 7; 
+	    minivan.fuelcap = 16; 
+	    minivan.mpg = 21; 
+	 
+	    // assign values to fields in sportscar 
+	    sportscar.passengers = 2; 
+	    sportscar.fuelcap = 14; 
+	    sportscar.mpg = 12; 
+	 
+	    gallons = minivan.fuelNeeded(dist); 
+	 
+	    System.out.println("To go " + dist + " miles minivan needs " + 
+	                       gallons + " gallons of fuel."); 
+	 
+	    gallons = sportscar.fuelNeeded(dist); 
+	 
+	    System.out.println("To go " + dist + " miles sportscar needs " + 
+	                       gallons + " gallons of fuel."); 
+	 
+	  } 
+
+}
+
+
