dae38c31422b795ae95a5ce12bd27bd9f297dad0
Innocent Harelimana <harera3@gmail.com>
2022-03-30 11:33:04 -0500

    add for each 1d and 2d

---
+++ javaguide/chapter05/Ex0507ForEach2D.java
@@ -0,0 +1,25 @@
+package ihh_javaguide05;
+
+//Using for-each style for on a 2d array. 
+
+public class Ex0507ForEach2D {
+	public static void main(String[] args) {
+		int sum = 0; 
+	    int[][] nums = new int[3][5]; 
+	    
+	    // give nums some values 
+	    for(int i = 0; i < 3; i++) 	    	
+	    	for(int j=0; j < 5; j++)	    		
+	    		nums[i][j] = (i+1)*(j+1); 
+	    
+	    // Use for-each for loop to display and sum the values. 
+	    for(int[] x : nums) { 
+	    	for(int y : x) { 
+	    		System.out.println("Value is: " + y); 
+		        sum += y; 
+	    	}	    		
+	    } 
+	    System.out.println("Summation: " + sum);
+	}
+}
+
