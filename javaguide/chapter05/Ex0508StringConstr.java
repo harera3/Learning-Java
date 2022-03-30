90e6b7f40c3f8d00c5f0a1eb027c6fb2b00948d0
Innocent Harelimana <harera3@gmail.com>
2022-03-30 17:02:49 -0500

    add string constructing

---
+++ javaguide/chapter05/Ex0508StringConstr.java
@@ -0,0 +1,18 @@
+package ihh_javaguide05;
+
+//Constructing String in 3 different ways. 
+
+public class Ex0508StringConstr {
+	public static void main(String[] args) {
+		// declare strings in various ways 
+	    String str1 = new String("Java strings are objects."); 
+	    String str2 = "They are constructed various ways."; 
+	    String str3 = new String(str2); 
+	 
+	    System.out.println(str1); 
+	    System.out.println(str2); 
+	    System.out.println(str3); 
+	}
+}
+
+
