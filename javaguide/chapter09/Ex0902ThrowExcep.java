51b660f31bb3f431fa043432b40122830327650a
Innocent Harelimana <harera3@gmail.com>
2022-03-31 18:51:48 -0500

    add throw exception

---
+++ javaguide/chapter09/Ex0902ThrowExcep.java
@@ -0,0 +1,18 @@
+package ihh_javaguide09;
+
+// Manually throwing an exception. 
+
+public class Ex0902ThrowExcep {
+	public static void main(String[] args) { 
+		try { 
+			System.out.println("Before throw.");
+			throw new ArithmeticException();
+		}
+		catch (ArithmeticException exc) {
+			// catch the exception 
+		    System.out.println("Exception caught.");
+		}
+		System.out.println("After try/catch block.");
+	}
+}
+
