d6ed3284a0fb89d460beb91eb5c3aedd7d4bdd5e
Innocent Harelimana <harera3@gmail.com>
2022-04-01 20:49:26 -0500

    add class structure

---
+++ ocpjava17/01_building_blocks/Ex01ClassStruc.java
@@ -0,0 +1,13 @@
+package ocp17chap01;
+
+public class Ex01ClassStruc {
+	String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String newName) {
+		name = newName;
+	}
+}
+
+
